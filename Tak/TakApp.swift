9ae631c284fe81295be430206c2f2d41567a2976
Yael Peet <yaelpeet@gmail.com>
2023-05-02 15:49:57 -0700

    Initial Commit

---
+++ Tak/TakApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakApp.swift
+//  Tak
+//
+//  Created by Yael Peet on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
