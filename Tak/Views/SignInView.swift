7262e76fe8d3f292425dd1decd193c0d2af85e93
Yael Peet <yaelpeet@gmail.com>
2023-05-17 15:31:26 -0700

    sign in view complete

--- Tak/Views/SignInView.swift
+++ Tak/Views/SignInView.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct SignInView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @ObservedObject var userViewModel: UserViewModel
+    
+    var body: some View {
+        VStack {
+            Text("Sign In")
+                .font(.largeTitle)
+            
+            TextField("Username", text: $username)
+                .padding()
+                .border(Color.gray, width: 0.5)
+            
+            SecureField("Password", text: $password)
+                .padding()
+                .border(Color.gray, width: 0.5)
+            
+            Button(action: {
+                userViewModel.signIn(username: username, password: password)
+            }) {
+                Text("Sign In")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
+    }
+}
