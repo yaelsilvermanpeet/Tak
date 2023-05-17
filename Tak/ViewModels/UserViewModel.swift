c804ee79fdcc8c18f1f669e0560718ebd1d5ff8d
Yael Peet <yaelpeet@gmail.com>
2023-05-17 15:53:38 -0700

    user view model complete

--- Tak/ViewModels/UserViewModel.swift
+++ Tak/ViewModels/UserViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+
+class UserViewModel: ObservableObject {
+    @Published var userProfile: UserProfile?
+    private var userService: UserService // Assume you have a UserService to handle sign in and sign up
+    
+    init(userService: UserService) {
+        self.userService = userService
+    }
+    
+    func signIn(username: String, password: String) {
+        userService.signIn(username: username, password: password) { result in
+            switch result {
+            case .success(let userProfile):
+                self.userProfile = userProfile
+            case .failure(let error):
+                print("Sign in failed with error: \(error)")
+            }
+        }
+    }
+    
+    func signUp(username: String, email: String, password: String) {
+        userService.signUp(username: username, email: email, password: password) { result in
+            switch result {
+            case .success(let userProfile):
+                self.userProfile = userProfile
+            case .failure(let error):
+                print("Sign up failed with error: \(error)")
+            }
+        }
+    }
+}
