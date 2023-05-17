fe38d64530472968a6f3736ed3c7ec13fdf86835
Yael Peet <yaelpeet@gmail.com>
2023-05-17 15:59:39 -0700

    game controller

--- Tak/Controllers/GameController.swift
+++ Tak/Controllers/GameController.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+
+class GameController {
+    var gameViewModel: GameViewModel
+    
+    init(gameViewModel: GameViewModel) {
+        self.gameViewModel = gameViewModel
+    }
+    
+    func placePiece(at position: Position, piece: Piece) {
+        gameViewModel.boardViewModel.placePiece(at: position, piece: piece)
+        if gameViewModel.boardViewModel.isGameOver().isOver {
+            print("Game over! Winner is \(gameViewModel.boardViewModel.isGameOver().winner?.name ?? "none")")
+        } else {
+            gameViewModel.switchPlayer()
+        }
+    }
+    
+    func movePiece(from startPosition: Position, to endPosition: Position, count: Int) {
+        gameViewModel.boardViewModel.movePiece(from: startPosition, to: endPosition, count: count, currentPlayer: gameViewModel.currentPlayer)
+        if gameViewModel.boardViewModel.isGameOver().isOver {
+            print("Game over! Winner is \(gameViewModel.boardViewModel.isGameOver().winner?.name ?? "none")")
+        } else {
+            gameViewModel.switchPlayer()
+        }
+    }
+}
