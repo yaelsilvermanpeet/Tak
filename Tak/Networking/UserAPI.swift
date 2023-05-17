2a6649c86fa6892e220d1b3d6cede3b691c02b84
Yael Peet <yaelpeet@gmail.com>
2023-05-17 16:08:55 -0700

    user api file complete

--- Tak/Networking/UserAPI.swift
+++ Tak/Networking/UserAPI.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class UserAPI {
+    let apiClient: APIClient
+    let baseURL = URL(string: "https://your-backend-service.com")!
+    
+    init(apiClient: APIClient) {
+        self.apiClient = apiClient
+    }
+    
+    func signIn(username: String, password: String, completion: @escaping (Result<UserProfile, Error>) -> Void) {
+        let signInURL = baseURL.appendingPathComponent("/sign-in")
+        var request = URLRequest(url: signInURL)
+        request.httpMethod = "POST"
+        let body = ["username": username, "password": password]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
+        
+        apiClient.performRequest(with: request, completion: completion)
+    }
+    
+    func signUp(username: String, email: String, password: String, completion: @escaping (Result<UserProfile, Error>) -> Void) {
+        let signUpURL = baseURL.appendingPathComponent("/sign-up")
+        var request = URLRequest(url: signUpURL)
+        request.httpMethod = "POST"
+        let body = ["username": username, "email": email, "password": password]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
+        
+        apiClient.performRequest(with: request, completion: completion)
+    }
+}
