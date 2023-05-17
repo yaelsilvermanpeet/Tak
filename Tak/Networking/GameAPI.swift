6764356deb2580a42dc110170b0e09c62320af44
Yael Peet <yaelpeet@gmail.com>
2023-05-17 16:14:14 -0700

    game API complete

--- Tak/Networking/GameAPI.swift
+++ Tak/Networking/GameAPI.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class GameAPI {
+    let apiClient: APIClient
+    let baseURL = URL(string: "https://your-backend-service.com")!
+    
+    init(apiClient: APIClient) {
+        self.apiClient = apiClient
+    }
+    
+    func startNewGame(completion: @escaping (Result<Game, Error>) -> Void) {
+        let startNewGameURL = baseURL.appendingPathComponent("/start-new-game")
+        var request = URLRequest(url: startNewGameURL)
+        request.httpMethod = "POST"
+        
+        apiClient.performRequest(with: request, completion: completion)
+    }
+    
+    func getGame(gameId: String, completion: @escaping (Result<Game, Error>) -> Void) {
+        let getGameURL = baseURL.appendingPathComponent("/games/\(gameId)")
+        let request = URLRequest(url: getGameURL)
+        
+        apiClient.performRequest(with: request, completion: completion)
+    }
+}
