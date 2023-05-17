e51c0e16fc11f010e65d908956de8cb048caddc6
Yael Peet <yaelpeet@gmail.com>
2023-05-17 15:37:25 -0700

    sign up view completed

--- Tak/Views/SignUpView.swift
+++ Tak/Views/SignUpView.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import SwiftUI
+struct SignUpView: View {
+    @State private var username: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @ObservedObject var userViewModel: UserViewModel
+    
+    var body: some View {
+        VStack {
+            Text("Sign Up")
+                .font(.largeTitle)
+            
+            TextField("Username", text: $username)
+                .padding()
+                .border(Color.gray, width: 0.5)
+            
+            TextField("Email", text: $email)
+                .padding()
+                .border(Color.gray, width: 0.5)
+            
+            SecureField("Password", text: $password)
+                .padding()
+                .border(Color.gray, width: 0.5)
+            
+            Button(action: {
+                userViewModel.signUp(username: username, email: email, password: password)
+            }) {
+                Text("Sign Up")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
+    }
+}
