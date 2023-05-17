9aaf66d6b887adb6cd4ad7eea7d287d812e29442
Yael Peet <yaelpeet@gmail.com>
2023-05-17 15:42:13 -0700

    game view  complete

--- Tak/Views/GameView.swift
+++ Tak/Views/GameView.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct GameView: View {
+    @ObservedObject var gameViewModel: GameViewModel
+    
+    var body: some View {
+        VStack {
+            Text("Game")
+                .font(.largeTitle)
+            
+            BoardView(board: gameViewModel.board)
+            
+            HStack {
+                Text("Player 1: \(gameViewModel.player1.name)")
+                    .font(.title2)
+                
+                Text("Player 2: \(gameViewModel.player2.name)")
+                    .font(.title2)
+            }
+            
+            Button(action: {
+                gameViewModel.startNewGame()
+            }) {
+                Text("Start New Game")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
+    }
+}
