1db915456584e469fa255afd05967420a8b9c84c
Yael Peet <yaelpeet@gmail.com>
2023-05-17 15:29:24 -0700

    user profile view added

--- Tak/Views/UserProfileView.swift
+++ Tak/Views/UserProfileView.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct UserProfileView: View {
+    @ObservedObject var userProfile: UserProfile
+    
+    var body: some View {
+        VStack {
+            Text("Username: \(userProfile.username)")
+                .font(.title)
+            
+            Text("Email: \(userProfile.email)")
+                .font(.title2)
+            
+            Text("Games Played: \(userProfile.gamesPlayed)")
+                .font(.title2)
+            
+            Text("Games Won: \(userProfile.gamesWon)")
+                .font(.title2)
+            
+            Button(action: {
+                // Action to start a new game
+            }) {
+                Text("Start New Game")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
+    }
+}
