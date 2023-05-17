83bc1ef1d3705b5d3c52a95a8d67cd2e13b2db91
Yael Peet <yaelpeet@gmail.com>
2023-05-17 15:51:20 -0700

    board view model initial complete

--- Tak/ViewModels/BoardViewModel.swift
+++ Tak/ViewModels/BoardViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+
+class BoardViewModel: ObservableObject {
+    @Published var board: Board
+    
+    init(board: Board) {
+        self.board = board
+    }
+    
+    func placePiece(at position: Position, piece: Piece) {
+        if board.isValidPosition(position) && board.hasRemainingPieces() {
+            board.placePiece(at: position, piece: piece)
+        }
+    }
+    
+    func movePiece(from startPosition: Position, to endPosition: Position, count: Int, currentPlayer: Player) {
+        if board.isValidPosition(startPosition) && board.isValidPosition(endPosition) {
+            _ = board.movePiece(from: startPosition, to: endPosition, count: count, currentPlayer: currentPlayer)
+        }
+    }
+    
+    func isGameOver() -> (isOver: Bool, winner: Player?) {
+        return board.isGameOver()
+    }
+}
