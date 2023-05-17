5a3dec2c82e0d21b1fae8ff1f33d52a3b7e9ecd5
Yael Peet <yaelpeet@gmail.com>
2023-05-17 16:05:27 -0700

    API client file complete

--- Tak/Networking/APIClient.swift
+++ Tak/Networking/APIClient.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+class APIClient {
+    let session: URLSession
+    
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    func performRequest<T: Decodable>(with request: URLRequest, completion: @escaping (Result<T, Error>) -> Void) {
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Data was not retrieved from request"]) as Error
+                completion(.failure(error))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let responseObject = try decoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(responseObject))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
