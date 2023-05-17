5682fc9db0fe6d0af45afa4ebb3ee76ffc370837
Yael Peet <yaelpeet@gmail.com>
2023-05-17 15:21:39 -0700

    updated board view

--- Tak/Views/BoardView.swift
+++ Tak/Views/BoardView.swift
@@ -6,5 +6,38 @@
 //
 
 import Foundation
-// new stupid line
+import SwiftUI
+
+struct BoardView: View {
+    @ObservedObject var board: Board
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<board.size, id: \.self) { i in
+                HStack {
+                    ForEach(0..<board.size, id: \.self) { j in
+                        SquareView(pieces: board.grid[i][j])
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct SquareView: View {
+    var pieces: [Piece]
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.gray)
+                .frame(width: 50, height: 50)
+            
+            if let topPiece = pieces.last {
+                Text(topPiece.type.rawValue)
+                    .foregroundColor(topPiece.player.color == .white ? .white : .black)
+            }
+        }
+    }
+}
 
